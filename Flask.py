c295be830461d2fc851f345cfb684c6cbe4a7cb2
Naseeeema <150550496+Naseeeema@users.noreply.github.com>
2024-05-03 11:15:09 +0400

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 import subprocess
-import re  # Import regular expression module
+import re  
 
 app = Flask(__name__)
 
@@ -20,10 +20,9 @@ def scan_endpoint():
     if not target:
         return jsonify({'error': 'Target URL is required'}), 400
 
-    # Full path to nettacker.py
     nettacker_path = '/home/kali/Nettacker/nettacker.py'
 
-    # Construct the Nettacker command
+
     command = f"python {nettacker_path} -m {module} -t {target}"
 
     scan_type = data.get('scan_type')
@@ -31,12 +30,11 @@ def scan_endpoint():
         command += f" -s {scan_type}"
 
     try:
-        # Execute the Nettacker command
+     
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         scan_results = output.decode() if output else error.decode()
 
-        # Parse scan results to extract vulnerabilities
         vulnerabilities = parse_scan_results(scan_results)
 
         return jsonify({'scan_results': scan_results.strip(), 'vulnerabilities': vulnerabilities})
@@ -44,11 +42,10 @@ def scan_endpoint():
         return jsonify({'error': str(e)}), 500
 
 def parse_scan_results(scan_results):
-    # Regular expression pattern to match vulnerabilities
-    vulnerability_pattern = r'Vulnerability: (.+)'  # Example pattern, modify as needed
 
-    # Use regular expression to find matches
-    matches = re.findall(vulnerability_pattern, scan_results)
+    vulnerability_pattern = r'Vulnerability: (.+)' 
+
+  matches = re.findall(vulnerability_pattern, scan_results)
 
     # Return list of vulnerabilities
     return matches
